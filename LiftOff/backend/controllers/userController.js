a8de4cbf8bc9a43ed43096e89c9fc00ab63dc2c5
Bryan Mirtil <166929786+Bryan2397@users.noreply.github.com>
2025-10-23 16:41:52 -0400

    Refactor userController with bcrypt and token handling
    Updated user registration and login logic to include password hashing and token generation. Added logout functionality.

--- LiftOff/backend/controllers/userController.js
+++ LiftOff/backend/controllers/userController.js
@@ -1,9 +1,12 @@
-const User = require('../models/Users');
+import User from '../models/Users.js';
+import bcrypt from "bcryptjs";
+import { generateToken } from '../lib/utils.js';
 
 //Register new user
-const registerUser = async (req, res) => {
+export const registerUser = async (req, res) => {
     try {
-        const { firstname, lastname, email, password } = req.body;
+        const { firstname, lastname, email, password, role } = req.body;
+        console.log(req.body);
 
         // Basic validation
         if(!firstname || !lastname || !email || !password) {
@@ -15,10 +18,35 @@ const registerUser = async (req, res) => {
         if (existingUser) {
             return res.status(400).json({ message: 'User already exists'});
         }
+        
+        //hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
-        //save user to database
-        const newUser = await User.create({ firstname, lastname, email, password });
-        res.status(201).json(newUser);
+        const newUser = new User({
+            firstname: firstname,
+            lastname: lastname,
+            email: email,
+            password: hashedPassword,
+            role: role,
+        });
+
+        if(newUser){
+            generateToken(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id:newUser._id,
+                firstname: newUser.firstname,
+                lastname: newUser.lastname,
+                email: newUser.email,
+                role: newUser.role,
+            });
+
+        }else{
+            res.status(400).json({ message: "Invalid user data"});
+        }
+                
         console.log("User registered: ", newUser);
     }catch (error) {
         res.status(500).json({ message: 'Server error', error });
@@ -26,20 +54,45 @@ const registerUser = async (req, res) => {
 };
 
 //Login user
-const loginUser = async (req, res) => {
+export const loginUser = async (req, res) => {
     try {
         const { email, password } = req.body;
         
         //find user
-        const user = await User.find({ email, password });
-        if (!user || user.password !== password) {
+        const user = await User.find({ email});
+        if (!user) {
             return res.status(400).json({ message: 'Invalid credentials' });
         }
 
+        const passwordCorrect = await bcrypt.compare(password, user.password);
+        if(!passwordCorrect){
+            return res.status(400).json({ message: "Invalid credentials"});
+        }
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            firstname: user.firstname,
+            lastname: user.lastname,
+            email: user.email,
+            role:user. role,
+        })
         console.log("Login successful", user);
     } catch (error) {
         res.status(500).json({ message: 'Server error', error });
     }
 };
 
-module.exports = { registerUser, loginUser };
+export const logout = (req, res) => {
+    try {
+        res.cookie("jwt", "", {maxAge: 0});
+        res.status(200).json({message: "Logged out Successfully" });
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+
+
+
