aaea5e3d132808b85bef3c34a8dc5bee22b2f121
Bryan Mirtil <166929786+Bryan2397@users.noreply.github.com>
2025-10-23 16:45:04 -0400

    Refactor server.js to use ES6 modules
    Refactor server.js to use ES6 imports and connect to MongoDB through a separate function which is what the lib folder is for.

--- LiftOff/backend/server.js
+++ LiftOff/backend/server.js
@@ -1,32 +1,33 @@
 // starts the express server and connects to MongoDB
 // register routes and middleware
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
 
-console.log("MONGO_URI: ", process.env.MONGO_URI);
+
+import dotenv from "dotenv"; // Load environment variables from .env file
+
+//import dependencies
+import express from "express"; //Web framework for Node.js
+import { connectDB } from "./lib/db.js"
+import cors from "cors"; //Middleware to allow cross-origin requests
+import userRoutes from "./routes/userRoutes.js";
+
+console.log("MONGO_URI: ", process.env.MONGO_URI); // Log the MongoDB URI to verify it's loaded correctly
+dotenv.config();
 const app = express();
 
 //Middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors()); //Enable CORS (Cross-Origin Resource Sharing) so frontend can communicate with backend
+
+app.use(express.json()); //Parse incoming JSON requests and put the parsed data in req.body
 
 
-//Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Error connecting to MongoDB', err));
 
-//Routes
-const userRoutes = require('./routes/userRoutes');
+//Prefix all user-related routes with /api/users
+// Example: POST /api/users/register
 app.use('/api/users', userRoutes);
 
 //start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; //Use port from environment or default to 5000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    connectDB();
 });
